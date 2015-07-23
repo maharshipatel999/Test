32368e78edd65b62114ec5528cc059a55a33302a
Maharshi Patel <maharshipatel999@gmail.com>
2015-07-23 11:00:29 -0400

    first commit

---
+++ app/src/main/java/com/hr/homedepot/lumdee/FIJI.java
@@ -0,0 +1,14 @@
+package com.hr.homedepot.lumdee;
+
+/**
+ * Created by HomeDepot on 7/23/15.
+ */
+public class FIJI {
+
+    private int plats;
+
+    public FIJI() {
+        plats = Integer.MAX_VALUE;
+    }
+
+}
